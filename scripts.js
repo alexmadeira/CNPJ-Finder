a0bcacb2dc6989e36eb13fc53099d179ab68e2ff
Alex Madeira <alex.c.madeira@gmail.com>
2022-08-08 12:34:35 -0300

    get

--- scripts.js
+++ scripts.js
@@ -1,13 +1,32 @@
-function getCnpj() {
+function getCnpj(cnpj) {
   $.ajax({
-    'url':'https://receitaws.com.br/v1/cnpj/28947502000143',
+    'url':`https://receitaws.com.br/v1/cnpj/${cnpj}`,
     "type":'GET',
     "dataType":'jsonp',
-    "success": function (data){
-      console.log(data)
-    }
+    "success": populate
   })
 }
-setTimeout(() => {
-  getCnpj()
-}, 2000);
+
+function populate(data) {
+
+  const cep = data.cep.replace(/\D/g, '').replace(/^([\d]{2})\.*([\d]{3})-*([\d]{3})/,"$1$2-$3");
+
+  console.log(cep)
+  const formatData = {...data, cep}
+
+  var empresa = $("#tpl #tpl-empresa").html();
+  var endereco = $("#tpl #tpl-endereco").html();
+
+  $("#empresa-output").html( Mustache.to_html(empresa, formatData) );
+  $("#endereco-output").html( Mustache.to_html(endereco, formatData) );
+}
+
+$( document ).ready(()=>{
+  $('.findCNPJ').click(()=>{
+    let cnpj = $('.cnpj').val().replace(/\D/g, '')
+    getCnpj(cnpj)
+  })
+});
+
+
+
